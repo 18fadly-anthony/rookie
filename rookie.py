3b13bd891d6d15abf2273fe4d0f092359ca8e1b6
Anthony Fadly <anthony_@teknik.io>
2021-02-15 09:33:54 -0800

    big changes

--- rookie.py
+++ rookie.py
@@ -6,39 +6,43 @@ import argparse
 home = os.path.expanduser('~')
 
 
+# Define General Functions
 def mkdirexists(dir):
     if not(os.path.isdir(dir)):
         os.mkdir(dir)
 
 
+# Define Package Manager Functions
 def init():
     mkdirexists(home + "/.rookie")
     mkdirexists(home + "/.rookie/definitions")
     mkdirexists(home + "/.rookie/generations")
+    mkdirexists(home + "/.rookie/store")
 
 
 def create(q):
-    mkdirexists(home + "/.rookie/definitions/" + q[0])
+    valid_package_types = ["script"] # TODO add appimage,tarball, git
+    # mkdirexists(home + "/.rookie/definitions/" + q[0]) # TODO validate type and url before creating generation
 
 
 def main():
 
     # Define Arguments
     parser = argparse.ArgumentParser(
-        description='''RookiePM: The Rookie Package Manager''',
+        description='''RookiePM: Yet Another Nix/Guix Inspired Package Manager''',
         epilog="""Copyright (C) TODO placeholder put something here""")
 
     parser.add_argument('--init', action='store_true', help='Setup RookiePM')
     parser.add_argument('--create', metavar=('<package>', '<type>', '<url>'), nargs=3, type=str, default="", help='Define <package> of <type> [script, tarball, appimage], with <url>')
-    parser.add_argument('--sync', metavar='<package>', nargs=1, type=str, default="", help='Sync (install and upgrade) <package>')
-    parser.add_argument('--sync-all', metavar='', help='Sync all installed packages')
+    parser.add_argument('--install', metavar='<package>', nargs=1, type=str, default="", help='Install <package>')
+    parser.add_argument('--update', metavar='<package>', nargs=1, type=str, default="", help='Update <package>')
     parser.add_argument('--remove', metavar='<package>', nargs=1, type=str, default="", help='Remove <package>')
-    parser.add_argument('--purge', metavar='<package>', nargs=1, type=str, default="", help='Remove <package> definition')
+    parser.add_argument('--remove-store', metavar='<package>', nargs=1, type=str, default="", help='Remove <package> and remove it from the store')
+    parser.add_argument('--remove-definition', metavar='<package>', nargs=1, type=str, default="", help='Remove <package>, and remove it from the store, and remove its definition')
     parser.add_argument('--list-packages', metavar='', help='List packages')
     parser.add_argument('--list-definitions', metavar='', help='List package definitions')
     parser.add_argument('--list-generations', metavar='', help='List generations')
     parser.add_argument('--switch', metavar='<generation>', nargs=1, type=int, default=0, help='Switch to <generation>')
-    parser.add_argument('--gc', metavar='', help='Garbage collect (delete) all past generation')
 
     args = parser.parse_args()
 
