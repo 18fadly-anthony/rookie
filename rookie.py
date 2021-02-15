c842b00e3be6574ea863c2508b154e901cb09cde
Anthony Fadly <anthony_@teknik.io>
2021-02-15 02:27:22 -0800

    start writing arguments

--- rookie.py
+++ rookie.py
@@ -1,10 +1,20 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 import sys
+import argparse
 
 
 def main():
-    pass # placeholder
+
+    # Define Arguments
+    parser = argparse.ArgumentParser(
+        description='''RookiePM: The Rookie Package Manager''',
+        epilog="""Copyright (C) TODO placeholder put something here""")
+
+    # parser.add_argument('--foo', type=int, default=42, help='FOO!')
+    # parser.add_argument('bar', nargs='*', default=[1, 2, 3], help='BAR!')
+
+    args=parser.parse_args()
 
 
 if __name__ == "__main__":
