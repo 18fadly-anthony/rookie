202eb781831f9b89664a1d55732ff51ccd7af721
Anthony Fadly <anthony_@teknik.io>
2021-02-15 02:22:47 -0800

    init

---
+++ rookie.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+
+import sys
+
+
+def main():
+    pass # placeholder
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\n' + "exit")
+        sys.exit(0)
