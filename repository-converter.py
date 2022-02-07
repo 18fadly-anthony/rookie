aa656d9eb0e748de86af423c484f9b2c24f25214
Anthony Fadly <anthony_@teknik.io>
2022-02-07 12:27:34 -0800

    run through directory

--- repository-converter.py
+++ repository-converter.py
@@ -8,6 +8,7 @@
 # Tool to convert old repo style to json
 
 import os
+import json
 import argparse
 
 home = os.path.expanduser('~')
@@ -22,6 +23,30 @@ def reverse_basename(path):
             l += i
             l += "/"
     return l
+
+
+def tree_files(path):
+    file_list = []
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            file_list.append(os.path.join(root, name))
+    return file_list
+
+
+def tree_dirs(path):
+    dir_list = []
+    for root, dirs, files in os.walk(path):
+        for name in dirs:
+            dir_list.append(os.path.join(root, name))
+    return dir_list
+
+
+def convert_repo(directory, output):
+    result = []
+    for i in tree_dirs(directory):
+        result.append({os.path.basename(i)})
+        #print(tree_files(i))
+    print(result)
 
 
 def main():
@@ -46,6 +71,8 @@ def main():
     if not os.path.exists(reverse_basename(args.output_file[0])):
         print("Error: directory not found " + reverse_basename(args.output_file[0]))
         exit()
+
+    convert_repo(args.directory[0], args.output_file[0])
 
 
 
